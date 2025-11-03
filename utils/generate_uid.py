292f19e6a2567ece43a20431df3b513c118e09a1
gamexdifficulty <gamexdifficulty@yahoo.com>
2025-11-03 23:41:04 +0100

    Added chars to uid generator

--- utils/generate_uid.py
+++ utils/generate_uid.py
@@ -5,7 +5,7 @@ def generate_uid(used_uids):
     for _ in range(100):
         new_uid = ""
         for _ in range(8):
-            new_uid += secrets.choice(string.digits)
+            new_uid += secrets.choice(string.digits+string.ascii_lowercase)
         
         if new_uid not in used_uids:
             used_uids.append(new_uid)
